3b5e2ad42608e3a5aee363504a642373d1378467
L4TN <matheus.dias17@fatec.sp.gov.br>
2021-07-01 21:42:35 -0300

    Apagando a main

--- Program.cs
+++ Program.cs
@@ -6,10 +6,7 @@ namespace DigitalInnovationOne
     {
         static void Main(string[] args)
         {
-            int numeroDeVezes=5;
-            for (int i=0;i<numeroDeVezes;i++)
-            {
-                Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+        
             }
 
         }
