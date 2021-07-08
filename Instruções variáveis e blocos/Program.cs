46a01c9e746c111a97b545ea920c2ca3fc9f3a15
LATN-dev <matheus.dias17@fatec.sp.gov.br>
2021-07-08 17:45:43 -0300

    Modificando a main

--- Instruções variáveis e blocos/Program.cs
+++ Instruções variáveis e blocos/Program.cs
@@ -4,9 +4,60 @@ namespace instruções_pratica
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Declaracoes()
         {
-            Console.WriteLine("Hello World!");
+            int a;
+            int b = 2, c=3; 
+            a = 1 ;
+            const int d = 4;
+            Console.WriteLine(a+b+c+d);
         }
+
+        static void InstrucaoIf(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Nenhum Argumento")
+            }
+            else if (args.Length == 1)
+            {
+                Console.WriteLine("Um Argumento")
+            }
+            else
+            {
+                Console.WriteLine($"{args.Length}Argumento")
+            }
+
+        }
+
+        static void InstrucaoSwitch(string[] arg)
+        {
+            int numeroDeArgumentos = args.Length;
+            switch (numeroDeArgumentos)
+            {
+                case 0:
+                    Console.WriteLine("Nenhum argumento");
+                    break;
+                case 1:
+                    Console.WriteLine("Um argumento");
+                    break;
+                default:
+                    Console.WriteLine($"{numeroDeArgumento} argumento");
+                    break;
+            }
+
+        }
+        static void InstrucaoWhile(string[] args)
+
+
+
+
+
+
+
+
     }
 }
+
+        
+
