5dd712f5208264c47b44c9c8d83fc96b373b9d81
LATN-dev <matheus.dias17@fatec.sp.gov.br>
2021-07-23 21:02:11 -0300

    Agora vai

---
+++ Desafios/SomaDoisNumeros.cs
@@ -0,0 +1,17 @@
+using System;
+
+class SomaDoisNumeros
+{
+    static void Main(string[] args)
+    {
+        //Declare Variables
+        int A = Convert.ToInt32(Console.ReadLine());
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        //Insira as variaveis de acordo com o solicitado
+        int SOMA = A+B;
+
+        //Print
+        Console.WriteLine("SOMA = {0}", SOMA);
+    }
+}
