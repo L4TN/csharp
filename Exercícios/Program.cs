229af2db0a63b39ac1abe07575b5ef2b15111cb4
L4TN <matheus.dias17@fatec.sp.gov.br>
2021-08-24 21:07:52 -0300

    Adicionando uma pasta de exercícios

---
+++ Exercícios/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercícios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("####CADASTRO DE INFORMAÇÕES####");
+            Console.WriteLine("Escolha uma das opções a seguir");
+            Console.WriteLine("1 - Registrar usuários");
+        }
+    }
+}
